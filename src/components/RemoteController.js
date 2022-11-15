ff34d6310747fd478c8a5c4dbb166f3d688cca58
Ryokuman <ryokuman@naver.com>
2022-11-16 07:38:11 +0900

    modify error

--- src/components/RemoteController.js
+++ src/components/RemoteController.js
@@ -55,10 +55,14 @@ function RemoteController(props) {
   const onZIndexChange = (e) => setZIndex(e?.target.value);
   const onImageChange = (e) => {
     const reader = new FileReader();
-    reader.readAsDataURL(e.target.files[0]);
-    reader.onload = () => {
-      setImgae(reader.result);
-    };
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = () => {
+        setImgae(reader.result);
+      };
+    } else {
+      setImgae(null);
+    }
   };
 
   const onClick = () => {
